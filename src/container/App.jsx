c4c3339584c557035218a768430ba96cff8f4f40
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-25 19:47:25 +0330

    :lipstick: Style: add 'Avineo', 'Monument Regular and Ultrabold' and 'Moresby' font family

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,9 +1,10 @@
 import "@unocss/reset/normalize.css";
 import "uno.css";
 /*
-	! use just in development mode
-	import "virtual:unocss-devtools";
+! use just in development mode
+import "virtual:unocss-devtools";
 */
+import "@/assets/scss/main.scss";
 
 function App() {
   return <></>;
