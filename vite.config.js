d95dde48998c8742ee9a614893a1445b858a6b0f
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-25 19:50:11 +0330

    :zap: Perf: setup absolute imports

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import react from "@vitejs/plugin-react-swc";
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
+  },
   plugins: [UnoCSS(), react()],
 });
